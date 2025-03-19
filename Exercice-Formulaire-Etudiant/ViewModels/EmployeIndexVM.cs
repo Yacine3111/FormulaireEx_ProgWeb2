6986f320251dc39ce5894e6b137aa15287861d1a
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-03-18 22:53:30 -0400

    EmployeIndexVM, EmployController, Employe View

---
+++ Exercice-Formulaire-Etudiant/ViewModels/EmployeIndexVM.cs
@@ -0,0 +1,25 @@
+﻿using Exercice_Formulaire_Etudiant.Models;
+
+namespace Exercice_Formulaire_Etudiant.ViewModels
+{
+    public class EmployeIndexVM
+    {
+        public string Name { get; set; }
+
+        public int age { get; set; }
+
+        public DateTimeOffset dateEmbauche { get; set; } = DateTimeOffset.Now;
+
+        public double annualSalary { get; set; }
+
+        public bool actualEmploye { get; set; }
+
+        public IList<Pays> pays { get; set; } = new List<Pays>();
+
+        public IList<Departement> departements { get; set; } = new List<Departement>();
+
+        public int paysId { get; set; }
+
+        public int departementId { get; set; }
+    }
+}
