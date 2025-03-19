d1329ca92417aab10490edb34af690ca9ba17a95
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-03-19 15:53:00 -0400

    restant

--- Exercice-Formulaire-Etudiant/Controllers/EmployeController.cs
+++ Exercice-Formulaire-Etudiant/Controllers/EmployeController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Exercice_Formulaire_Etudiant.Data;
+using Exercice_Formulaire_Etudiant.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Exercice_Formulaire_Etudiant.Controllers
 {
     public class EmployeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public EmployeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View(new EmployeIndexVM());
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeIndexVM vm)
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
+
--- Exercice-Formulaire-Etudiant/ViewModels/EmployeIndexVM.cs
+++ Exercice-Formulaire-Etudiant/ViewModels/EmployeIndexVM.cs
@@ -1,25 +1,35 @@
 ﻿using Exercice_Formulaire_Etudiant.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Exercice_Formulaire_Etudiant.ViewModels
 {
     public class EmployeIndexVM
     {
+        [DisplayName("Nom")]
         public string Name { get; set; }
 
+        [DisplayName("Age")]
         public int age { get; set; }
 
-        public DateTimeOffset dateEmbauche { get; set; } = DateTimeOffset.Now;
-
+        [DisplayName("Date d'embauche")]
+        [DataType(DataType.Date)]
+        public DateTime dateEmbauche { get; set; } = DateTime.Today;
+        [DisplayName("Salaire annuel")]
         public double annualSalary { get; set; }
 
+        [DisplayName("Présentement à l’emploi?")]
         public bool actualEmploye { get; set; }
 
         public IList<Pays> pays { get; set; } = new List<Pays>();
 
         public IList<Departement> departements { get; set; } = new List<Departement>();
 
+        [DisplayName("Pays d'origine")]
         public int paysId { get; set; }
-
+        [DisplayName("Département de travail")]
         public int departementId { get; set; }
+
+
     }
 }
